a3e65b89e4fce6f95af1edcb5ea68e5f6f839f45
Himubey <himubey@hotmail.com>
2023-07-31 17:29:58 +0530

    change API server

--- script.js
+++ script.js
@@ -5,14 +5,31 @@ speakBtn = document.querySelector(".speak"),
 copyBtn = document.querySelector(".copy");
 
 
+// function randomQuote(){
+// const header = new Headers({ "Access-Control-Allow-Origin": "*" });
+// const api_url ="https://zenquotes.io/api/random";
+
+// async function getapi(url)
+// {
+   
+//   const response = await fetch(url);
+//   var data = await response.json();
+//   console.log(data);
+// }
+
+// getapi(api_url);
+
+// }
 
 // random quote function
 
 function randomQuote(){
+   const header = new Headers({ "Access-Control-Allow-Origin": "*" });
+   mode:'no-cors'
    quoteBtn.classList.add("loading");
    quoteBtn.innerText = "Loading Quote...";
  // fetching random quotes/data from API and parsing it into JavaScript object
-    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+    fetch("https://zenquotes.io/api/random").then(res => res.json()).then(result =>{
     console.log(result)
     quoteText.innerText = result.content;
     authorName.innerText = result.author;
